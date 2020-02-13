349ab9e09b56bae8987997c8a600073162068622
Chris Idema <chris.idema@eletronsquare.com>
2020-02-13 14:36:14 +0100

    -added more attempts

---
+++ components/component1/project_include.cmake
@@ -0,0 +1,12 @@
+cmake_minimum_required(VERSION 3.5)
+
+#set(EXTRA_COMPONENT_DIRS components/component1/components)
+#set(EXTRA_COMPONENT_DIRS components)
+#set(EXTRA_COMPONENT_DIRS /home/e2/E2/nested_components/components/component1/components)
+#set(EXTRA_COMPONENT_DIRS COMPONENT_DIR/components)
+
+#idf_build_set_property(EXTRA_COMPONENT_DIRS COMPONENT_DIR/components)
+
+#set(EXTRA_COMPONENT_DIRS COMPONENT_DIR/components APPEND)
+
+
